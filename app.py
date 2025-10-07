f122f4c4211e97c6fa4fac857396c276dd4abe80
Rafael Dantas <rafael.hfdantas@outlook.com>
2025-10-06 22:09:16 -0300

    test: habit-tracker.db is working

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from database import get_db
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def hello():
+    cur = get_db().cursor()
+    cur.execute("SELECT * FROM users WHERE id = ?", (1,))
+    user_data = cur.fetchone()
+    print(f'teste {user_data}')
+
+    
     return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import g
 
-DATABASE = '/path/to/database.db'
+DATABASE = 'habit-tracker.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -9,8 +9,8 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
